c060573fedf6ebcca4e0e2ac7c816a6aac26b164
Maely Gauthier <mg3342@raijin1.(none)>
2018-07-09 16:55:06 +1000

    fix how to extract the JOBID for PBS

--- IndivAnalysis.sh
+++ IndivAnalysis.sh
@@ -5,8 +5,6 @@ BAM=$1
 COV=$2
 TRANSP_TYPE=$3
 TRANSP_FILE=/g/data1a/jp48/scripts/MELTv2.1.4/me_refs/1KGP_Hg19/${TRANSP_TYPE}_MELT.zip
-#/g/data1a/jp48/scripts/MELTv2.1.4/me_refs/1KGP_Hg19/LINE1_MELT.zip
-#/g/data1a/jp48/scripts/MELTv2.1.4/prior_files/LINE1.1KGP.sites.vcf
 PRIORS_FILE=/g/data1a/jp48/scripts/MELTv2.1.4/prior_files/${TRANSP_TYPE}.1KGP.sites.vcf
 OUTPUTDIR=/g/data1a/jp48/MELT/IndivAnalysis/${TRANSP_TYPE}
 
--- preprocess.sh
+++ preprocess.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
+##
+#Maely Gauthier
+#09 July 2018
+#First step to run MELT pipeline
+##
 
 
 # Preprocess generates three files, all as suffixes to the current bam file:
-#sorted.bam.disc – discordant pairs from the current BAM file
-#sorted.bam.disc.bai – index of these discordant pairs
-#sorted.bam.disc.fq – fastq version of all discordant pairs for MEI alignment
+#sorted.bam.disc discordant pairs from the current BAM file
+#sorted.bam.disc.bai index of these discordant pairs
+#sorted.bam.disc.fq fastq version of all discordant pairs for MEI alignment
+
+#These will be added to the folder where the bam files reside
 
 
 
 ##Get the bam name and coverage from the job submission command-line like: ./IndivAnalysis.sh <Preprocessed_Sample.bam>
+
 BAM=$1
 
 java -Xmx4G  -jar /g/data1a/jp48/scripts/MELTv2.1.4/MELT.jar Preprocess -bamfile $BAM -h /g/data1a/jp48/scripts/human_g1k_v37_decoy.fasta
