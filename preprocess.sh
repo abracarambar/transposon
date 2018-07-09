9e3b8faea6376adfcbb4a9483e87b2b9b2aad2b3
Maely Gauthier <mg3342@raijin1.(none)>
2018-07-09 15:45:13 +1000

    code improvement including deriving individual analysis step for each transposon element

--- preprocess.sh
+++ preprocess.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#PBS -V
-#PBS -N MELT
-#PBS -j oe
-#PBS -m ae
-#PBS -M m.gauthier@garvan.org.au
-#PBS -q express
-#PBS -l ncpus=8
-#PBS -l mem=4gb
-#PBS -l walltime=24:00:00
- 
-module load java
-module load bowtie2
- 
+
+
+# Preprocess generates three files, all as suffixes to the current bam file:
+#sorted.bam.disc – discordant pairs from the current BAM file
+#sorted.bam.disc.bai – index of these discordant pairs
+#sorted.bam.disc.fq – fastq version of all discordant pairs for MEI alignment
+
+
+
 ##Get the bam name and coverage from the job submission command-line like: ./IndivAnalysis.sh <Preprocessed_Sample.bam>
 BAM=$1
- 
+
 java -Xmx4G  -jar /g/data1a/jp48/scripts/MELTv2.1.4/MELT.jar Preprocess -bamfile $BAM -h /g/data1a/jp48/scripts/human_g1k_v37_decoy.fasta
