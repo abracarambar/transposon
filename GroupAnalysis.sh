c2d43e9d567ad8d8097e34dd9e843e3724b89ff8
Maely Gauthier <mg3342@raijin2.(none)>
2018-07-17 16:10:10 +1000

    add next steps of the pipeline: group analysis and genotyping

---
+++ GroupAnalysis.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+TRANSP_TYPE=$1
+TRANSP_FILE=/g/data1a/jp48/scripts/MELTv2.1.4/me_refs/1KGP_Hg19/${TRANSP_TYPE}_MELT.zip
+PRIORS_FILE=/g/data1a/jp48/scripts/MELTv2.1.4/prior_files/${TRANSP_TYPE}.1KGP.sites.vcf
+#provide location of files
+FILE_LOC=/g/data1a/jp48/scripts/MELTv2.1.4/${TRANSP_TYPE}_with_priors.txt
+INDOUTPUTDIR=/g/data1a/jp48/MELT/IndivAnalysis/${TRANSP_TYPE}
+GROUPOUTPUTDIR=/g/data1a/jp48/MELT/GroupAnalysis/${TRANSP_TYPE}
+
+java -Xmx6G -jar /g/data1a/jp48/scripts/MELTv2.1.4/MELT.jar GroupAnalysis\
+     -discoverydir $GROUPOUTPUTDIR \
+     -h /g/data1a/jp48/scripts/human_g1k_v37_decoy.fasta \
+     -n /g/data1a/jp48/scripts/MELTv2.1.4/add_bed_files/1KGP_Hg19/hg19.genes.bed \
+     -r 150 \
+     -t $FILE_LOC \
+     -w $INDOUTPUTDIR
