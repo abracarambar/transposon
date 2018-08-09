5604ddafb5be5528aaa1fe45c6cf24bbb1b63868
maely <maelygauthier@gmail.com>
2018-08-09 16:28:40 +1000

    double variable typo

--- IndivAnalysis.sh
+++ IndivAnalysis.sh
@@ -16,7 +16,7 @@ TRANSP_TYPE=$3
 OUTPUTDIR=$4
 TRANSP_FILE=/g/data1a/jp48/scripts/MELTv2.1.4/me_refs/1KGP_Hg19/${TRANSP_TYPE}_MELT.zip
 PRIORS_FILE=/g/data1a/jp48/scripts/MELTv2.1.4/prior_files/${TRANSP_TYPE}.1KGP.sites.vcf
-OUTPUTDIR=/g/data1a/jp48/MELT/${TRANSP_TYPE}
+#OUTPUTDIR=/g/data1a/jp48/MELT/${TRANSP_TYPE}
 
 java -Xmx6G -jar /g/data1a/jp48/scripts/MELTv2.1.4/MELT.jar IndivAnalysis \
      -t $TRANSP_FILE $PRIORS_FILE \
