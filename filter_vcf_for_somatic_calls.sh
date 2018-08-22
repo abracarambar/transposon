3860cfa208d8e50df8d9b41f3a773c5fb231aeb5
maely <maelygauthier@gmail.com>
2018-08-22 15:04:06 +1000

    fix the filtering for the somatic

--- filter_vcf_for_somatic_calls.sh
+++ filter_vcf_for_somatic_calls.sh
@@ -27,7 +27,7 @@ fi
 
 #VCF_base=`basename $VCF .vcf`
 
-bcftools view -t ^hs37d5  -s $NORMAL,$TUMOUR -f '%LINE\n' -e '%FILTER=="ac0" || %FILTER=="lc" || LP+RP<4 || SR<4 || ASSESS<4' -U ${TRANS_DIR}/$VCF.gz  | sed 's/0\/0\:/0\/0\ /g' | sed 's/0\/1\:/0\/1\ /g' | sed 's/1\/1\:/1\/1\ /g' | sed 's/1\/0\:/1\/0\ /g' | tr  ' ' '\t' | awk '{if (($10=="0/1" && $12=="0/0") || ($10=="1/0" && $12=="0/0")) print}' > ${TRANS_DIR}/${VCF}_${SAMPLE}_somatic.vcf 
+bcftools view -t ^hs37d5  -s $NORMAL,$TUMOUR -f '%LINE\n' -e '%FILTER=="ac0" || %FILTER=="lc" || LP+RP<4 || SR<4 || ASSESS<4' -U ${TRANS_DIR}/$VCF.gz  | sed 's/0\/0\:/0\/0\ /g' | sed 's/0\/1\:/0\/1\ /g' | sed 's/1\/1\:/1\/1\ /g' | sed 's/1\/0\:/1\/0\ /g' | sed 's/.\/.\:/.\/.\ /g' | tr  ' ' '\t' | awk '{if (($10=="0/0" && $12=="0/1") || ($10=="0/1" && $12=="0/0") || ($10=="0/1" && $12=="./.") || ($10=="0/1" && $12=="1/1") || ($10=="1/1" && $12=="0/1")) print}' > ${TRANS_DIR}/${VCF}_${SAMPLE}_somatic.vcf 
 
 #sleep 5
 
